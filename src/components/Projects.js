c6f9d62d9b8d40fefc91e86d279da08c70ac9515
bijodev <bijodev1@gmail.com>
2025-10-30 12:17:12 +0530

    updating project knowurchurch

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -127,7 +127,7 @@ const ProjectCard = ({ project }) => {
             <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
             </svg>
-            Live Demo
+            Live
           </a>
         ) : (
           <span className="text-yellow-400 text-sm">🚀 {project.status}</span>
